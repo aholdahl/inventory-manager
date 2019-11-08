0fe190a7d049b23d8b39a5448f4c2ec28e8e279d
Allyson Holdahl <allyson.holdahl@gmail.com>
2019-11-08 15:05:20 -0600

    production build

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,13 +1,27 @@
 const pg = require('pg');
 
-const Pool = pg.Pool;
-const pool = new Pool({
-    database: 'inventory_manager',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-});
+if (process.env.DATABASE_URL) {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+    config = {
+        user: auth[0],
+        password: auth[1],
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        ssl: true,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    } || {
+            database: 'sample_database_name',
+            host: 'localhost',
+            port: 5432,
+            max: 10,
+            idleTimeoutMillis: 30000
+        }
+}
+
+const pool = new pg.Pool(config);
 
 pool.on('connect', () => {
     console.log('pool connected to database');
@@ -15,6 +29,7 @@ pool.on('connect', () => {
 
 pool.on('error', () => {
     console.log('error connecting pool to database');
+    process.exit(-1);
 });
 
 module.exports = pool;
